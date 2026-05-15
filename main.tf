9629f142585e10fc47853727ef56228f8e2500b6
emman216 <88820528+emman216@users.noreply.github.com>
2026-05-15 12:06:40 +0800

    update name

--- main.tf
+++ main.tf
@@ -39,8 +39,8 @@ module "blog_sg" {
   ingress_rules = ["http-80-tcp","http-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  ingress_rules = ["all-all"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog"{
