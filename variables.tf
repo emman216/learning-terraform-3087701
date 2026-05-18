62c34d0d621d1ca7ca620df92bf1eff0b7359a39
emman216 <88820528+emman216@users.noreply.github.com>
2026-05-18 08:00:15 +0800

    corrected variable name

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "instance_type" {
   default     = "t3.nano"
 }
 
-variable "aws_ami" "app_ami" {
+variable "ami_filter" {
   description = "Name filter and owner for AMI"
 
   type = object ({
